1513121a63a1dbdc1f2b7563145095975a2f79bc
fcd <1352288+femnad@users.noreply.github.com>
2024-01-06 06:52:20 +0300

    don't display boost information

--- src/notify/mod.rs
+++ src/notify/mod.rs
@@ -20,33 +20,16 @@ fn get_icon(volume: f32) -> String {
     format!("audio-volume-{}-symbolic", classifier)
 }
 
-fn get_boost_level(volume: f32) -> u32 {
-    let vol_i = (volume * 100.0) as u32;
-    vol_i / 100
-}
-
 pub fn volume(volume: f32) {
-    let mut normalized_volume = volume;
-    let boosting = volume > 1.0;
-    let boost_level = get_boost_level(volume);
-    if boosting {
-        normalized_volume = volume - (boost_level as f32 * 1.0);
-    }
-    let boosted = if boosting {
-        format!(" [{boost_level}x boost]").to_string()
-    } else {
-        String::new()
-    };
-
     let icon = get_icon(volume);
-    let vol_int = (normalized_volume * 100.0) as u32;
-    let summary = format!("Volume{boosted}");
+    let vol_int = (volume * 100.0) as u32;
+
     Notification::new()
         .appname("volume")
         .urgency(Urgency::Low)
         .hint(Hint::CustomInt("value".to_string(), vol_int as i32))
         .icon(icon.as_str())
-        .summary(summary.as_str())
+        .summary("Volume")
         .show()
         .expect("error sending notification");
 }
